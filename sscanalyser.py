6988e035b59e763e4b6d709a173f1f639f89da76
Padhaibook <119981275+Padhaibook@users.noreply.github.com>
2022-12-07 05:42:51 +0530

    Update sscanalyser.py

--- sscanalyser.py
+++ sscanalyser.py
@@ -4,5 +4,8 @@ st.title('SSC Result analyser')
 
 Q = st.slider('How many questions you attemted in the entire paper?', 0,100,0,1)
 
-A = st.slider('What is the percentage accuracy? ( in % )',0,100,0,5)
+A = st.slider('What is the percentage OF CORRECT answers? ( in % )',0,100,0,5)
 
+estimated = Q * (A/100)
+
+"your approximate score is",estimated
