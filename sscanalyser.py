b19fd41419f8a7abcb42bcc7a06ee7b4ba9f455a
Padhaibook <119981275+Padhaibook@users.noreply.github.com>
2022-12-07 05:49:27 +0530

    Update sscanalyser.py

--- sscanalyser.py
+++ sscanalyser.py
@@ -6,6 +6,8 @@ Q = st.slider('How many questions you attemted in the entire paper?', 0,100,0,1)
 
 A = st.slider('What is the percentage OF CORRECT answers? ( in % )',0,100,0,5)
 
-estimated = Q * (A/100)
+wrong = 100-A
+estimated = (2*(Q * (A/100)))-(wrong/100)*0.5
+
 
 "your approximate score is",estimated
