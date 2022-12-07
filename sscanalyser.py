abaf26fca4133d070cb54baa063dbf52e7f496bf
Padhaibook <119981275+Padhaibook@users.noreply.github.com>
2022-12-07 06:08:16 +0530

    Update sscanalyser.py

--- sscanalyser.py
+++ sscanalyser.py
@@ -6,7 +6,7 @@ Q = st.slider('How many questions you attempted in the entire paper?', 0,100,0,1
 
 A = st.slider('What is the percentage OF CORRECT answers? ( in % )',0,100,0,5)
 
-estimated = (A/100*Q)*2-(100-A)/100)*0.5*Q
+estimated = ((A/100*Q)*2)-((100-A)/100*0.5*Q)
 
 
 "your approximate score is",estimated
