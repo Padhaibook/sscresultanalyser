f7a2e2f5b3f6ebc54e7e7128d3f8a5db695c0ee6
Padhaibook <119981275+Padhaibook@users.noreply.github.com>
2022-12-07 06:06:21 +0530

    Update sscanalyser.py

--- sscanalyser.py
+++ sscanalyser.py
@@ -2,12 +2,11 @@ import streamlit as st
 
 st.title('SSC Result analyser')
 
-Q = st.slider('How many questions you attemted in the entire paper?', 0,100,0,1)
+Q = st.slider('How many questions you attempted in the entire paper?', 0,100,0,1)
 
 A = st.slider('What is the percentage OF CORRECT answers? ( in % )',0,100,0,5)
 
-wrong = 100-A
-estimated = (2*(Q * (A/100)))-(wrong/100)*0.5
+estimated = (A/100*Q)*2-((100-A)/100))*0.5*Q
 
 
 "your approximate score is",estimated
